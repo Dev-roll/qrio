c25ecb0b1519a4d788c1fc2825d0e2b29f96a6e7
keigomichi <hundredacrewood1165g7@gmail.com>
2022-12-09 15:26:28 +0900

    test: `widget_test.dart` 内のエラー解消

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:qrio/main.dart';
+import 'package:qrio/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Qrio());
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
