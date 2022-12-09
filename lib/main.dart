7029a6937bcfb430c34c9657005b5fddb87118b9
Keigo Kato <hundredacrewood1165g7@gmail.com>
2022-12-09 12:46:19 +0900

    feat: 日本語へのローカライズ設定 (#5)
    * feat: 日本語へのローカライズ設定
    
    * style: 不要なカンマを削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -11,10 +12,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   final theme = prefs.getString('theme') ?? 'system';
-  runApp(ProviderScope(
-    overrides: [themeProvider.overrideWith((ref) => stringToThemeMode(theme))],
-    child: const Qrio(),
-  ));
+  runApp(ProviderScope(overrides: [
+    themeProvider.overrideWith((ref) => stringToThemeMode(theme))
+  ], child: const Qrio()));
 }
 
 class Qrio extends ConsumerWidget {
@@ -24,20 +24,25 @@ class Qrio extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Consumer(
         builder: ((context, ref, _) => MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'qrio',
-              themeMode: ref.watch(themeProvider),
-              theme: ThemeData(
-                  useMaterial3: true,
-                  // TODO: Set the correct value for colorSchemeSeed
-                  colorSchemeSeed: Colors.blue,
-                  brightness: Brightness.light),
-              darkTheme: ThemeData(
-                  useMaterial3: true,
-                  // TODO: Set the correct value for colorSchemeSeed
-                  colorSchemeSeed: Colors.blue,
-                  brightness: Brightness.dark),
-              home: const Home(),
-            )));
+            debugShowCheckedModeBanner: false,
+            title: 'qrio',
+            localizationsDelegates: const [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [Locale('ja', 'JP')],
+            themeMode: ref.watch(themeProvider),
+            theme: ThemeData(
+                useMaterial3: true,
+                // TODO: Set the correct value for colorSchemeSeed
+                colorSchemeSeed: Colors.blue,
+                brightness: Brightness.light),
+            darkTheme: ThemeData(
+                useMaterial3: true,
+                // TODO: Set the correct value for colorSchemeSeed
+                colorSchemeSeed: Colors.blue,
+                brightness: Brightness.dark),
+            home: const Home())));
   }
 }
--- lib/src/widgets/select_theme_dialog.dart
+++ lib/src/widgets/select_theme_dialog.dart
@@ -14,36 +14,36 @@ class SelectThemeDialog extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return AlertDialog(
-      title: const Text("テーマの選択"),
-      content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: selectThemeOption
-              .map((e) => RadioListTile<ThemeMode>(
-                  title: Text(e['label']),
-                  value: e['value'],
-                  groupValue: ref.watch(temporaryThemeProvider),
-                  onChanged: (ThemeMode? value) =>
-                      ref.read(temporaryThemeProvider.notifier).state = value!))
-              .toList(growable: false)),
-      actions: <Widget>[
-        TextButton(
-          child: const Text('キャンセル'),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        TextButton(
-          child: const Text('OK'),
-          onPressed: () async {
-            final selectedTheme = ref.watch(temporaryThemeProvider);
-            ref.read(themeProvider.notifier).state = selectedTheme;
-            final SharedPreferences prefs =
-                await SharedPreferences.getInstance();
-            await prefs.setString('theme', themeModeToString(selectedTheme));
-            // TODO: Refer to https://dart-lang.github.io/linter/lints/use_build_context_synchronously.html
-            // ignore: use_build_context_synchronously
-            Navigator.of(context).pop();
-          },
-        ),
-      ],
-    );
+        title: const Text("テーマの選択"),
+        content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: selectThemeOption
+                .map((e) => RadioListTile<ThemeMode>(
+                    title: Text(e['label']),
+                    value: e['value'],
+                    groupValue: ref.watch(temporaryThemeProvider),
+                    onChanged: (ThemeMode? value) => ref
+                        .read(temporaryThemeProvider.notifier)
+                        .state = value!))
+                .toList(growable: false)),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('キャンセル'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          TextButton(
+              child: const Text('OK'),
+              onPressed: () async {
+                final selectedTheme = ref.watch(temporaryThemeProvider);
+                ref.read(themeProvider.notifier).state = selectedTheme;
+                final SharedPreferences prefs =
+                    await SharedPreferences.getInstance();
+                await prefs.setString(
+                    'theme', themeModeToString(selectedTheme));
+                // TODO: Refer to https://dart-lang.github.io/linter/lints/use_build_context_synchronously.html
+                // ignore: use_build_context_synchronously
+                Navigator.of(context).pop();
+              })
+        ]);
   }
 }
