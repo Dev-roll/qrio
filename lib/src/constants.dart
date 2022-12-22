873c765e0525b768bde4ca2d3312defcbf563986
notchcoder <kokiuaxdx@gmail.com>
2022-12-22 17:42:38 +0900

    feat: リンクを識別しやすく

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -65,3 +65,6 @@ SelectOption getOptionFromValue(dynamic value, List<SelectOption> options) {
   }
   throw Error();
 }
+
+RegExp linkFormat = RegExp(
+    r'((https?:\/\/)|(https?:www\.)|(www\.))[a-zA-Z0-9-]{1,256}\.[a-zA-Z0-9]{2,6}(\/[a-zA-Z0-9亜-熙ぁ-んァ-ヶ()@:%_\+.~#?&\/=-]*)?');
