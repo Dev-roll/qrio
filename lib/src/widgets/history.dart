ebdcec4a6c7f6158e6839fb70887e165bdd27292
notchcoder <kokiuaxdx@gmail.com>
2023-02-28 11:52:02 +0900

    feat: レイアウト変更

--- lib/src/widgets/history.dart
+++ lib/src/widgets/history.dart
@@ -53,210 +53,230 @@ class History extends ConsumerWidget {
       error: (err, _) => Text(err.toString()), //エラー時
       loading: () => const CircularProgressIndicator(), //読み込み時
       data: (historyList) {
-        return Column(
+        return Stack(
           children: [
-            const SizedBox(
-              height: 16,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Column(
               children: [
-                Container(
-                  width: 32,
-                  height: 4,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(2),
-                    color:
-                        Theme.of(context).colorScheme.outline.withOpacity(0.8),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
+                const SizedBox(height: 16),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const SizedBox(
-                      width: 28,
-                    ),
-                    Text(
-                      '履歴',
-                      style: TextStyle(
+                    Container(
+                      width: 32,
+                      height: 4,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(2),
                         color: Theme.of(context)
                             .colorScheme
-                            .onBackground
-                            .withOpacity(0.7),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 24,
-                    ),
-                    Text(
-                      '${List.from(historyList).length} 件',
-                      style: TextStyle(
-                        color: Theme.of(context)
-                            .colorScheme
-                            .onBackground
-                            .withOpacity(0.7),
+                            .outline
+                            .withOpacity(0.8),
                       ),
                     ),
                   ],
                 ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    IconButton(
-                      onPressed: List.from(historyList).isEmpty
-                          ? null
-                          : () async {
-                              await showDialog(
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  icon:
-                                      const Icon(Icons.delete_outline_rounded),
-                                  title: const Text('削除'),
-                                  content: Text(
-                                    'すべての履歴を削除しますか？',
-                                    style: TextStyle(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onSurfaceVariant,
-                                    ),
-                                  ),
-                                  actions: [
-                                    TextButton(
-                                        onPressed: () =>
-                                            {Navigator.pop(context)},
-                                        onLongPress: null,
-                                        child: const Text('キャンセル')),
-                                    TextButton(
-                                      onPressed: () async {
-                                        Navigator.pop(context);
-                                        deleteHistory();
-                                      },
-                                      onLongPress: null,
-                                      child: const Text('すべて削除'),
-                                    ),
-                                  ],
-                                ),
-                              );
-                            },
-                      icon: const Icon(Icons.delete_outline_rounded),
-                      disabledColor: Theme.of(context)
-                          .colorScheme
-                          .onBackground
-                          .withOpacity(0.3),
-                      padding: const EdgeInsets.all(16.0),
-                    ),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            if (List.from(historyList).isEmpty)
-              Column(
-                children: [
-                  Icon(
-                    Icons.update_disabled_rounded,
-                    size: 120,
-                    color: Theme.of(context)
-                        .colorScheme
-                        .onBackground
-                        .withOpacity(0.3),
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    '読み取り・作成の履歴はありません',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.onBackground,
-                    ),
-                  ),
-                ],
-              ),
-            for (var e in List.from(historyList.reversed))
-              InkWell(
-                onTap: () async {
-                  if (await canLaunchUrl(Uri.parse(e))) {
-                    launchURL(e);
-                  } else {
-                    Clipboard.setData(
-                      ClipboardData(text: e),
-                    ).then((value) {
-                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        BottomSnackBar(
-                          context,
-                          'クリップボードにコピーしました',
-                          icon: Icons.library_add_check_rounded,
+                    Row(
+                      children: [
+                        const SizedBox(
+                          width: 28,
                         ),
-                      );
-                    });
-                  }
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      width: 28,
-                    ),
-                    Expanded(
-                      child: Text(
-                        '$e',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: linkFormat.hasMatch(e.toString())
-                              ? Theme.of(context).colorScheme.secondary
-                              : Theme.of(context).colorScheme.onBackground,
-                          decoration: linkFormat.hasMatch(e.toString())
-                              ? TextDecoration.underline
-                              : TextDecoration.none,
+                        Text(
+                          '履歴',
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onBackground
+                                .withOpacity(0.7),
+                          ),
                         ),
-                        overflow: TextOverflow.fade,
-                        maxLines: 1,
-                        softWrap: false,
-                      ),
+                        const SizedBox(
+                          width: 24,
+                        ),
+                        Text(
+                          '${List.from(historyList).length} 件',
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onBackground
+                                .withOpacity(0.7),
+                          ),
+                        ),
+                      ],
                     ),
                     Row(
                       children: [
                         IconButton(
-                          onPressed: () {
-                            Share.share(
-                              e,
-                              subject: 'QR I/O の履歴共有',
-                            );
-                          },
-                          icon: const Icon(Icons.share_rounded),
+                          onPressed: List.from(historyList).isEmpty
+                              ? null
+                              : () async {
+                                  await showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                      icon: const Icon(
+                                          Icons.delete_outline_rounded),
+                                      title: const Text('削除'),
+                                      content: Text(
+                                        'すべての履歴を削除しますか？',
+                                        style: TextStyle(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onSurfaceVariant,
+                                        ),
+                                      ),
+                                      actions: [
+                                        TextButton(
+                                            onPressed: () =>
+                                                {Navigator.pop(context)},
+                                            onLongPress: null,
+                                            child: const Text('キャンセル')),
+                                        TextButton(
+                                          onPressed: () async {
+                                            Navigator.pop(context);
+                                            deleteHistory();
+                                          },
+                                          onLongPress: null,
+                                          child: const Text('すべて削除'),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                          icon: const Icon(Icons.delete_outline_rounded),
+                          disabledColor: Theme.of(context)
+                              .colorScheme
+                              .onBackground
+                              .withOpacity(0.3),
                           padding: const EdgeInsets.all(16.0),
                         ),
-                        IconButton(
-                          onPressed: () {
-                            showModalBottomSheet(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return DataBottomSheet(data: e, ref: ref);
-                              },
-                              backgroundColor: Colors.transparent,
-                            );
-                          },
-                          icon: const Icon(Icons.more_vert_rounded),
-                          padding: const EdgeInsets.all(16.0),
-                        ),
-                        const SizedBox(
-                          width: 8,
-                        ),
+                        const SizedBox(width: 8),
                       ],
                     ),
                   ],
                 ),
-              ),
-            const SizedBox(
-              height: 28,
+                Divider(
+                  height: 1,
+                  thickness: 1,
+                  color:
+                      Theme.of(context).colorScheme.outline.withOpacity(0.25),
+                ),
+                if (List.from(historyList).isEmpty)
+                  Column(
+                    children: [
+                      const SizedBox(height: 8),
+                      Icon(
+                        Icons.update_disabled_rounded,
+                        size: 120,
+                        color: Theme.of(context)
+                            .colorScheme
+                            .onBackground
+                            .withOpacity(0.3),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        '読み取り・作成の履歴はありません',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
             ),
+            Container(
+              margin: const EdgeInsets.only(top: 77),
+              height: MediaQuery.of(context).size.height -
+                  (MediaQuery.of(context).padding.top + 133),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 8),
+                    for (var e in List.from(historyList.reversed))
+                      InkWell(
+                        onTap: () async {
+                          if (await canLaunchUrl(Uri.parse(e))) {
+                            launchURL(e);
+                          } else {
+                            Clipboard.setData(
+                              ClipboardData(text: e),
+                            ).then((value) {
+                              ScaffoldMessenger.of(context)
+                                  .hideCurrentSnackBar();
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                BottomSnackBar(
+                                  context,
+                                  'クリップボードにコピーしました',
+                                  icon: Icons.library_add_check_rounded,
+                                ),
+                              );
+                            });
+                          }
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              width: 28,
+                            ),
+                            Expanded(
+                              child: Text(
+                                '$e',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: linkFormat.hasMatch(e.toString())
+                                      ? Theme.of(context).colorScheme.secondary
+                                      : Theme.of(context)
+                                          .colorScheme
+                                          .onBackground,
+                                  decoration: linkFormat.hasMatch(e.toString())
+                                      ? TextDecoration.underline
+                                      : TextDecoration.none,
+                                ),
+                                overflow: TextOverflow.fade,
+                                maxLines: 1,
+                                softWrap: false,
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {
+                                    Share.share(
+                                      e,
+                                      subject: 'QR I/O の履歴共有',
+                                    );
+                                  },
+                                  icon: const Icon(Icons.share_rounded),
+                                  padding: const EdgeInsets.all(16.0),
+                                ),
+                                IconButton(
+                                  onPressed: () {
+                                    showModalBottomSheet(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return DataBottomSheet(
+                                            data: e, ref: ref);
+                                      },
+                                      backgroundColor: Colors.transparent,
+                                    );
+                                  },
+                                  icon: const Icon(Icons.more_vert_rounded),
+                                  padding: const EdgeInsets.all(16.0),
+                                ),
+                                const SizedBox(
+                                  width: 8,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    const SizedBox(height: 24),
+                  ],
+                ),
+              ),
+            )
           ],
         );
       }, //データ受け取り時
