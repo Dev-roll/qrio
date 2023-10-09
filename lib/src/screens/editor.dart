bd588ffa3c1f34169d1fedd2e53e872e649adb39
notchcoder <kokiuaxdx@gmail.com>
2023-10-09 19:34:17 +0900

    chore: 編集画面のデザイン変更

--- lib/src/screens/editor.dart
+++ lib/src/screens/editor.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qrio/src/constants.dart';
+import 'package:qrio/src/utils.dart';
 import 'package:qrio/src/widgets/config_items.dart';
 import 'package:qrio/src/widgets/qr_code_preview.dart';
 
@@ -22,12 +23,46 @@ class Editor extends StatelessWidget {
                 ),
               ),
               QrCodePreview(),
-              const Expanded(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 4),
-                  child: SingleChildScrollView(
-                    child: ConfigItems(),
-                  ),
+              Expanded(
+                child: Stack(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).size.height *
+                                defaultSheetHeight(context) +
+                            10,
+                      ),
+                      padding: const EdgeInsets.only(top: 4),
+                      child: const SingleChildScrollView(
+                        padding: EdgeInsets.only(bottom: 64),
+                        child: ConfigItems(),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 64,
+                      margin: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).size.height *
+                                defaultSheetHeight(context) +
+                            10,
+                      ),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Theme.of(context)
+                                .colorScheme
+                                .background
+                                .withOpacity(0),
+                            Theme.of(context).colorScheme.background,
+                            Theme.of(context).colorScheme.background,
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
